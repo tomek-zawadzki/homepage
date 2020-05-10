05a8b643d3000108b0c3291bd1707c75438f030c
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-05-10 19:57:29 +0200

    use query to change heading

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const name = "Tomek";
-const age = "30";
+const age = "29";
 
 console.log(name);
 console.log(age);
 console.log(`ELO ELO 3210, jestem ${name} i mam ${age} lat.`);
+
+const heading = document.querySelector('.week-description__title--js');
+heading.innerHTML = `Notatki z pierwszego tygodnia.`;
+
+console.log(heading);
+
+const allDom = document.querySelector('body');
+
+console.log(allDom)
