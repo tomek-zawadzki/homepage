45f5ce2229ce043e3277cb61e9529cc1d178a0c7
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-05-16 23:01:19 +0200

    adding new content with new styles and adding greet function

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ console.log(`ELO ELO 3210, jestem ${name} i mam ${age} lat.`);
 const heading = document.querySelector('.week-description__title--js');
 heading.innerHTML = `Notatki z pierwszego tygodnia.`;
 
-console.log(heading);
 
 const allDom = document.querySelector('body');
 
 console.log(allDom)
+
+function greet(name, age) {
+    return `Witam Cię przyjacielu jestem ${name} i liczę sobie ${age} wiosen`
+}
+
+console.log(greet(name, age));
