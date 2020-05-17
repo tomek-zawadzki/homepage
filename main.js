b3f7e36c18bd32cc25cc096bab3c44915ff68ac1
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-05-17 18:05:40 +0200

    adding new content, articles and sections

--- main.js
+++ main.js
@@ -5,28 +5,27 @@ console.log(name);
 console.log(age);
 console.log(`ELO ELO 3210, jestem ${name} i mam ${age} lat.`);
 
-const heading = document.querySelector('.week-description__title--js');
+const heading = document.querySelector(".week-description__title--js");
 heading.innerHTML = `Notatki z pierwszego tygodnia.`;
 
-
-const allDom = document.querySelector('body');
+const allDom = document.querySelector("body");
 
 console.log(allDom);
 
 function greetOlder(name, age) {
-    return `Witam Cię przyjacielu w stylu fat arrow, jestem ${name} i liczę sobie ${age} wiosen`;
-};
+  return `Witam Cię przyjacielu w stylu fat arrow, jestem ${name} i liczę sobie ${age} wiosen`;
+}
 
 console.log(greetOlder(name, age));
 
 const greetOld = (name, age) => {
-    return `Witam Cię przyjacielu w stylu fat arrow, jestem ${name} i liczę sobie ${age} wiosen`;
+  return `Witam Cię przyjacielu w stylu fat arrow, jestem ${name} i liczę sobie ${age} wiosen`;
 };
 
-
 console.log(greetOld(name, age));
-console.log(greetOld('joachimek', 13));
+console.log(greetOld("joachimek", 13));
 
-const greet = (name, age) => `Witam Cię przyjacielu w stylu skróconego fat arrow, jestem ${name} i liczę sobie ${age} wiosen`;
+const greet = (name, age) =>
+  `Witam Cię przyjacielu w stylu skróconego fat arrow, jestem ${name} i liczę sobie ${age} wiosen`;
 
-console.log(greet('Patrynio', 100));
+console.log(greet("Patrynio", 100));
