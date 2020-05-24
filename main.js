cf2cf10c9cd56be0de3c3b75b0b0c8bfa1ade7e3
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-05-24 21:40:09 +0200

    adding hamburger menu

--- main.js
+++ main.js
@@ -127,3 +127,11 @@ const buttonThird = document.querySelector('.mouse-over--js');
 console.log(buttonThird)
 
 buttonThird.addEventListener('mouseover', myMouseOver )
+
+const buttonHamburger =  document.querySelector('.navigation__button--js')
+
+buttonHamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js')
+    nav.classList.toggle('navigation--open')
+
+})
